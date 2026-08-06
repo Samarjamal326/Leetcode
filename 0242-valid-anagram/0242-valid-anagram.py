84aea2e2598bef5504dc87235bf89a39294a6ad5
Samar Jamal <Samarjamal326@gmail.com>
2026-08-06 21:14:47 +0530

    Time: 18 ms (19.61%), Space: 20.3 MB (10.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,16 +3,10 @@ from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        freqs = defaultdict(int)
-        freqt = defaultdict(int)
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
 
-        for i in s:
-            freqs[i] += 1
-        print(freqs)
-        for j in t:
-            freqt[j] += 1
-
-        if freqs == freqt:
+        if sorted_s == sorted_t:
             return True
         else:
             return False
