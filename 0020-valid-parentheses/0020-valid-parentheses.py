0fab3d635d33dbd95c2ec3150931528e03d42ae1
Samar Jamal <Samarjamal326@gmail.com>
2026-08-08 21:22:33 +0530

    Time: 3 ms (32.03%), Space: 19.5 MB (23.89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,32 +1,31 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+
+        if len(s) % 2 != 0:
+            return False
         
         opened = []
 
         pairs = {
-            ')': '(',
-            ']': '[',
-            '}': '{'
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
         }
 
-        if len(s)%2 != 0:
-            return False
-
-        for i in range(0, len(s)):
+        for i in range(len(s)):
             if s[i] == '(' or s[i] == '[' or s[i] == '{':
                 opened.append(s[i])
+            
             elif len(opened) == 0:
                 return False
-            elif s[i] == ')' or s[i] == ']' or s[i] == '}':
-                if len(opened) == 0:
-                    return False
 
-                if opened[-1] != pairs[s[i]]:
-                    return False
-
+            elif opened[-1] == pairs[s[i]]:
                 opened.pop()
-                
-        if len(opened) == 0:
-            return True
-        else:
-            return False
+            
+            else:
+                return False
+        
+        return len(opened) == 0
+
+
+
