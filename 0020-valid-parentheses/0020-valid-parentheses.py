b65c42f50f2f8b7a316d17dbb3be5588fcd2084b
Samar Jamal <Samarjamal326@gmail.com>
2026-08-10 20:58:30 +0530

    Time: 0 ms (100%), Space: 19.3 MB (63.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,29 +3,26 @@ class Solution:
 
         if len(s) % 2 != 0:
             return False
-        
+
         opened = []
 
         pairs = {
-            ')' : '(',
-            ']' : '[',
-            '}' : '{'
+            ')': '(',
+            ']': '[',
+            '}': '{'
         }
 
         for i in range(len(s)):
             if s[i] == '(' or s[i] == '[' or s[i] == '{':
                 opened.append(s[i])
-            
+
             elif len(opened) == 0:
                 return False
 
             elif opened[-1] == pairs[s[i]]:
                 opened.pop()
-            
+
             else:
                 return False
-        
+
         return len(opened) == 0
-
-
-
